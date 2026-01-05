f1af9db8a3eff3b3b7a7c06d0cb5eb5c5f00fdf0
Igor da Silva Dias <rogigor1712@gmail.com>
2026-01-05 15:28:47 -0300

    template next.js mas com typescript

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page(){
+  return(
+    <div>
+      <h1>lembre-se de mudar o nome desse projeto em package.json</h1>
+      <h1>lembre-se de rodar npm install no terminal integrado para instalar as dependências necessárias</h1>
+    </div>
+  )
+}
